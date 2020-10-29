2baaf651abd888bcd577d4883cdaa1bd420c7a28
Anil Ramisetty <aramisetty@bridgetree.com>
2020-10-29 22:32:41 +0530

    added the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AccountComponent } from './account/account.component';
+import { CustomDirective } from './custom-directive/custom-directive-component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent,
+    CustomDirective,
+    DashboardComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
